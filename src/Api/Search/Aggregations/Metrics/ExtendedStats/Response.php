a3b7b688359a6f0885806461326d777e78b3fa2b
Maxim Grabazey <mgrabazey@gmail.com>
2018-03-19 22:11:50 +0200

    implement

--- src/Api/Search/Aggregations/Metrics/ExtendedStats/Response.php
+++ src/Api/Search/Aggregations/Metrics/ExtendedStats/Response.php
@@ -8,6 +8,51 @@ use CodeSolo\Elasticsearch\Exception\InvalidRawData;
 class Response extends AbstractResponse
 {
     /**
+     * @var float
+     */
+    private $count;
+
+    /**
+     * @var float
+     */
+    private $min;
+
+    /**
+     * @var float
+     */
+    private $max;
+
+    /**
+     * @var float
+     */
+    private $avg;
+
+    /**
+     * @var float
+     */
+    private $sum;
+
+    /**
+     * @var float
+     */
+    private $sumOfSquares;
+
+    /**
+     * @var float
+     */
+    private $variance;
+
+    /**
+     * @var float
+     */
+    private $stdDeviation;
+
+    /**
+     * @var Response\StdDeviationBounds
+     */
+    private $stdDeviationBounds;
+
+    /**
      * @param array $data
      * @return Response|static
      * @throws InvalidRawData
@@ -27,6 +72,88 @@ class Response extends AbstractResponse
             throw new InvalidRawData();
         }
         $instance = new static();
+        $instance->count = $data['count'];
+        $instance->min = $data['min'];
+        $instance->max = $data['max'];
+        $instance->avg = $data['avg'];
+        $instance->sum = $data['sum'];
+        $instance->sumOfSquares = $data['sum_of_squares'];
+        $instance->variance = $data['variance'];
+        $instance->stdDeviation = $data['std_deviation'];
+        $instance->stdDeviationBounds = Response\StdDeviationBounds
+            ::fromRawData($data['std_deviation_bounds']);
         return $instance;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCount(): float
+    {
+        return $this->count;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMin(): float
+    {
+        return $this->min;
+    }
+
+    /**
+     * @return float
+     */
+    public function getMax(): float
+    {
+        return $this->max;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAvg(): float
+    {
+        return $this->avg;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSum(): float
+    {
+        return $this->sum;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSumOfSquares(): float
+    {
+        return $this->sumOfSquares;
+    }
+
+    /**
+     * @return float
+     */
+    public function getVariance(): float
+    {
+        return $this->variance;
+    }
+
+    /**
+     * @return float
+     */
+    public function getStdDeviation(): float
+    {
+        return $this->stdDeviation;
+    }
+
+    /**
+     * @return Response\StdDeviationBounds
+     */
+    public function getStdDeviationBounds(): Response\StdDeviationBounds
+    {
+        return $this->stdDeviationBounds;
     }
 }
