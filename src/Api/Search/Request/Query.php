6706c20a5803e7e6ee0fd863ce7784a61be5540f
Maxim Grabazey <maxim.grabazey@comodo.od.ua>
2018-04-13 22:02:30 +0300

    implement

--- src/Api/Search/Request/Query.php
+++ src/Api/Search/Request/Query.php
@@ -3,7 +3,34 @@
 namespace CodeSolo\Elasticsearch\Api\Search\Request;
 
 use CodeSolo\Elasticsearch\Api\AbstractRequest;
+use CodeSolo\Elasticsearch\Api\Search\Request\Query\AbstractClause;
 
 class Query extends AbstractRequest
 {
+    /**
+     * @var AbstractClause[]
+     */
+    private $clauses = [];
+
+    /**
+     * @return array
+     */
+    public function toDsl(): array
+    {
+        $dsl = parent::toDsl();
+        foreach ($this->clauses as $clause) {
+            $dsl = array_merge($dsl, $clause->toDsl());
+        }
+        return $dsl;
+    }
+
+    /**
+     * @param AbstractClause $clause
+     * @return Query|static
+     */
+    public function addClause(AbstractClause $clause): Query
+    {
+        $this->clauses[] = $clause;
+        return $this;
+    }
 }
