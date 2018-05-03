92040a62deff42a4b4841c0f9fc0d9df27c9b99c
Maxim Grabazey <mgrabazey@gmail.com>
2018-05-03 22:59:44 +0300

    implement

--- src/Api/Search/Common/Aggregations/AbstractResponse.php
+++ src/Api/Search/Common/Aggregations/AbstractResponse.php
@@ -2,7 +2,9 @@
 
 namespace CodeSolo\Elasticsearch\Api\Search\Common\Aggregations;
 
-abstract class AbstractResponse
+use CodeSolo\Elasticsearch\Api\AbstractResponse as Base;
+
+abstract class AbstractResponse extends Base
 {
 
 }
--- src/Api/Search/Common/Response/Aggregations.php
+++ src/Api/Search/Common/Response/Aggregations.php
@@ -7,60 +7,63 @@ use CodeSolo\Elasticsearch\Api\Search\Common\Aggregations as Aggs;
 
 class Aggregations
 {
-    const MAP = [
-        AggregationsType::METRICS_AVG => Aggs\Metrics\Avg\Response::class,
-        AggregationsType::METRICS_CARDINALITY => Aggs\Metrics\Cardinality\Response::class,
-        AggregationsType::METRICS_EXTENDED_STATS => Aggs\Metrics\ExtendedStats\Response::class,
-        AggregationsType::METRICS_GEO_BOUNDS => Aggs\Metrics\GeoBounds\Response::class,
-        AggregationsType::METRICS_GEO_CENTROID => Aggs\Metrics\GeoCentroid\Response::class,
-        AggregationsType::METRICS_MAX => Aggs\Metrics\Max\Response::class,
-        AggregationsType::METRICS_MIN => Aggs\Metrics\Min\Response::class,
-        AggregationsType::METRICS_PERCENTILES => Aggs\Metrics\Percentiles\Response::class,
+    /**
+     * @var string[]
+     */
+    private $map = [
+        AggregationsType::METRICS_AVG              => Aggs\Metrics\Avg\Response::class,
+        AggregationsType::METRICS_CARDINALITY      => Aggs\Metrics\Cardinality\Response::class,
+        AggregationsType::METRICS_EXTENDED_STATS   => Aggs\Metrics\ExtendedStats\Response::class,
+        AggregationsType::METRICS_GEO_BOUNDS       => Aggs\Metrics\GeoBounds\Response::class,
+        AggregationsType::METRICS_GEO_CENTROID     => Aggs\Metrics\GeoCentroid\Response::class,
+        AggregationsType::METRICS_MAX              => Aggs\Metrics\Max\Response::class,
+        AggregationsType::METRICS_MIN              => Aggs\Metrics\Min\Response::class,
+        AggregationsType::METRICS_PERCENTILES      => Aggs\Metrics\Percentiles\Response::class,
         AggregationsType::METRICS_PERCENTILE_RANKS => Aggs\Metrics\PercentileRanks\Response::class,
-        AggregationsType::METRICS_SCRIPTED_METRIC => '',
-        AggregationsType::METRICS_STATS => Aggs\Metrics\Stats\Response::class,
-        AggregationsType::METRICS_SUM => Aggs\Metrics\Sum\Response::class,
-        AggregationsType::METRICS_TOP_HITS => '',
-        AggregationsType::METRICS_VALUE_COUNT => Aggs\Metrics\ValueCount\Response::class,
+        AggregationsType::METRICS_SCRIPTED_METRIC  => '',
+        AggregationsType::METRICS_STATS            => Aggs\Metrics\Stats\Response::class,
+        AggregationsType::METRICS_SUM              => Aggs\Metrics\Sum\Response::class,
+        AggregationsType::METRICS_TOP_HITS         => '',
+        AggregationsType::METRICS_VALUE_COUNT      => Aggs\Metrics\ValueCount\Response::class,
 
-        AggregationsType::BUCKET_ADJACENCY_MATRIX => 'adjacency_matrix',
-        AggregationsType::BUCKET_CHILDREN => 'children',
-        AggregationsType::BUCKET_COMPOSITE => 'composite',
-        AggregationsType::BUCKET_DATE_HISTOGRAM => 'date_histogram',
-        AggregationsType::BUCKET_DATE_RANGE => 'date_range',
-        AggregationsType::BUCKET_DIVERSIFIED_SAMPLER => 'diversified_sampler',
-        AggregationsType::BUCKET_FILTER => 'filter',
-        AggregationsType::BUCKET_FILTERS => 'filters',
-        AggregationsType::BUCKET_GEO_DISTANCE => 'geo_distance',
-        AggregationsType::BUCKET_GEOHASH_GRID => 'geohash_grid',
-        AggregationsType::BUCKET_GLOBAL => 'global',
-        AggregationsType::BUCKET_HISTOGRAM => 'histogram',
-        AggregationsType::BUCKET_IP_RANGE => 'ip_range',
-        AggregationsType::BUCKET_MISSING => 'missing',
-        AggregationsType::BUCKET_NESTED => 'nested',
-        AggregationsType::BUCKET_RANGE => 'range',
-        AggregationsType::BUCKET_REVERSE_NESTED => 'reverse_nested',
-        AggregationsType::BUCKET_SAMPLER => 'sampler',
-        AggregationsType::BUCKET_SIGNIFICANT_TERMS => 'significant_terms',
-        AggregationsType::BUCKET_SIGNIFICANT_TEXT => 'significant_text',
-        AggregationsType::BUCKET_TERMS => 'terms',
+        AggregationsType::BUCKET_ADJACENCY_MATRIX    => Aggs\Bucket\AdjacencyMatrix\Response::class,
+        AggregationsType::BUCKET_CHILDREN            => Aggs\Bucket\Children\Response::class,
+        AggregationsType::BUCKET_COMPOSITE           => '',
+        AggregationsType::BUCKET_DATE_HISTOGRAM      => '',
+        AggregationsType::BUCKET_DATE_RANGE          => '',
+        AggregationsType::BUCKET_DIVERSIFIED_SAMPLER => '',
+        AggregationsType::BUCKET_FILTER              => '',
+        AggregationsType::BUCKET_FILTERS             => '',
+        AggregationsType::BUCKET_GEO_DISTANCE        => '',
+        AggregationsType::BUCKET_GEOHASH_GRID        => '',
+        AggregationsType::BUCKET_GLOBAL              => '',
+        AggregationsType::BUCKET_HISTOGRAM           => '',
+        AggregationsType::BUCKET_IP_RANGE            => '',
+        AggregationsType::BUCKET_MISSING             => '',
+        AggregationsType::BUCKET_NESTED              => '',
+        AggregationsType::BUCKET_RANGE               => '',
+        AggregationsType::BUCKET_REVERSE_NESTED      => '',
+        AggregationsType::BUCKET_SAMPLER             => '',
+        AggregationsType::BUCKET_SIGNIFICANT_TERMS   => '',
+        AggregationsType::BUCKET_SIGNIFICANT_TEXT    => '',
+        AggregationsType::BUCKET_TERMS               => Aggs\Bucket\Terms\Response::class,
 
-        AggregationsType::PIPELINE_AVG_BUCKET => 'avg_bucket',
-        AggregationsType::PIPELINE_DERIVATIVE => 'derivative',
-        AggregationsType::PIPELINE_MAX_BUCKET => 'max_bucket',
-        AggregationsType::PIPELINE_MIN_BUCKET => 'min_bucket',
-        AggregationsType::PIPELINE_SUM_BUCKET => 'sum_bucket',
-        AggregationsType::PIPELINE_STATS_BUCKET => 'stats_bucket',
-        AggregationsType::PIPELINE_EXTENDED_STATS_BUCKET => 'extended_stats_bucket',
-        AggregationsType::PIPELINE_PERCENTILES_BUCKET => 'percentiles_bucket',
-        AggregationsType::PIPELINE_MOVING_AVG => 'moving_avg',
-        AggregationsType::PIPELINE_CUMULATIVE_SUM => 'cumulative_sum',
-        AggregationsType::PIPELINE_BUCKET_SCRIPT => 'bucket_script',
-        AggregationsType::PIPELINE_BUCKET_SELECTOR => 'bucket_selector',
-        AggregationsType::PIPELINE_BUCKET_SORT => 'bucket_sort',
-        AggregationsType::PIPELINE_SERIAL_DIFF => 'serial_diff',
+        AggregationsType::PIPELINE_AVG_BUCKET            => '',
+        AggregationsType::PIPELINE_DERIVATIVE            => '',
+        AggregationsType::PIPELINE_MAX_BUCKET            => '',
+        AggregationsType::PIPELINE_MIN_BUCKET            => '',
+        AggregationsType::PIPELINE_SUM_BUCKET            => '',
+        AggregationsType::PIPELINE_STATS_BUCKET          => '',
+        AggregationsType::PIPELINE_EXTENDED_STATS_BUCKET => '',
+        AggregationsType::PIPELINE_PERCENTILES_BUCKET    => '',
+        AggregationsType::PIPELINE_MOVING_AVG            => '',
+        AggregationsType::PIPELINE_CUMULATIVE_SUM        => '',
+        AggregationsType::PIPELINE_BUCKET_SCRIPT         => '',
+        AggregationsType::PIPELINE_BUCKET_SELECTOR       => '',
+        AggregationsType::PIPELINE_BUCKET_SORT           => '',
+        AggregationsType::PIPELINE_SERIAL_DIFF           => '',
 
-        AggregationsType::MATRIX_STATS => 'matrix_stats',
+        AggregationsType::MATRIX_STATS => Aggs\Matrix\MatrixStats\Response::class,
     ];
 
     /**
@@ -101,20 +104,13 @@ class Aggregations
         if (!array_key_exists($name, $this->decodedItems)) {
             $data = $this->rawData[$name];
 
-            switch ($type) {
-                case AggregationsType::BUCKET_TERMS:
-                    $item = Aggs\Bucket\Terms\Response::fromRawData($data);
-                    break;
-
-                case AggregationsType::METRICS_AVG:
-                    $item = Aggs\Metrics\Avg\Response::fromRawData($data);
-                    break;
-
-                default:
-                    // TODO change it
-                    throw new \Exception();
+            if (!array_key_exists($type, $this->map)) {
+                // TODO change it
+                throw new \Exception();
             }
-            $this->decodedItems[$name] = $item;
+            /** @var Aggs\AbstractResponse $responseClass */
+            $responseClass = $this->map[$type];
+            $this->decodedItems[$name] = $responseClass::fromRawData($data);
         }
         return $this->decodedItems[$name];
     }
